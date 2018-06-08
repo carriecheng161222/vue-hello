ed1a05a337ec5da801d369fad7968a4a5386f205
carriecheng161222 <39619917+carriecheng161222@users.noreply.github.com>
2018-06-08 17:33:59 +0800

    Update item1.vue

--- src/pages/item1.vue
+++ src/pages/item1.vue
@@ -1,44 +1,17 @@
 <template>
   <section>  
-    <div class="container" ref="mybox">  
+    <div class="container">  
       <span>Message: {{Msg1}}</span>
-      <p>Computed reversed message: "{{ reversedMsg1() }}"</p>
 
-      <!--双大括号会将数据解释为普通文本，而非 HTML 代码。为了输出真正的 HTML，你需要使用 v-html 指令-->
-      <p>Using v-html directive: <span v-html="Msg1"></span></p>
-
-      <div>{{ fullName}}</div>
+      <p>reversed message: "{{ reversedMsg1() }}"</p>
     </div>  
   </section>
 </template>
-<script>
-  var _obj = document.getElementsByClassName('container'); 
-  var _w = document.documentElement.clientWidth; 
-  var _h = document.documentElement.clientHeight; 
-  //console.log(_w,_h);
-
-  
+<script>   
   export default {
     name: 'item1', 
-    // data(){return {Msg1:'Content1'}},
-    data:{
-      firstName: 'Foo',
-      lastName: 'Bar'      
-    },
-    computed: {
-      fullName: function () {
-        return this.firstName + ' ' + this.lastName
-      }
-    },
-    mounted() {
-      this.init();
-    },
+    data(){return {Msg1:'Content1'}},
     methods: {  
-      init() {  
-        const self = this;  
-        this.$refs.mybox.style.width = _w + "px";  
-        self.$refs.mybox.style.height = (_h - 40 - 50) + "px";  
-      },
       reversedMsg1: function () { 
         return this.Msg1.split('').reverse().join('') 
       }
@@ -46,5 +19,5 @@
   }
 </script>
 <style scoped>
-  .container {color: #ffffff;text-align: center;color: red;width: 0;height: 0;}
+  .container {color: #ffffff;text-align: center;color: red;width: 100%;height: 0;}
 </style>
